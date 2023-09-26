0382b9f4e07476eb6fdfb7915f30a03e3111fdae
AnaJuliaMM <ana.martins_2004@outlook.com>
2023-09-25 22:28:17 -0300

    feat(core): change the user in the django auth

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Definição do usuário do aplicação
+AUTH_USER_MODEL = "todo.UserEntity"
+
 
 # Application definition
 
@@ -76,20 +79,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     ##### ELEPHANT SQL
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hklnzipd',
-        'USER': 'hklnzipd',
-        'PASSWORD': 'FcssfgsdsdkG-3cFPzoXCYa5Yvh_A186',
-        'HOST': 'hansken.db.elephantsql.com',
-        'PORT': '5432',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'hklnzipd',
+    #     'USER': 'hklnzipd',
+    #     'PASSWORD': 'FcssfgsdsdkG-3cFPzoXCYa5Yvh_A186',
+    #     'HOST': 'hansken.db.elephantsql.com',
+    #     'PORT': '5432',
+    # }
     
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
